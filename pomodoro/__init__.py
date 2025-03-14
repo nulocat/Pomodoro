003393f379cd8b7edd5fff10e53d9baaf3d9dbdb
NuloCat <nulocat@proton.me>
2025-03-14 14:35:59 -0300

    feat: add initial Flask app with builtin modularize schems and add debug run script

---
+++ pomodoro/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, Blueprint
+from pomodoro import views
+
+def load_routes(instance: Flask | Blueprint):
+    for url, func in views.ROUTES.items():
+        instance.add_url_rule(url, view_func=func)
+    return instance
+
+def create_app(): # for use like a standalone app
+    app = Flask(__name__)
+    app = load_routes(app)
+    return app
+
+def create_blueprint(): # for use like a subapp
+    bp = Blueprint('pomodoro', __name__, template_folder='templates')
+    bp = load_routes(bp)
+    return bp
+
+
